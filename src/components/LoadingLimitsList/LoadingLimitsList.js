6d46802b6d85a01feb2a62c3ed6d581a080e0cc9
Kate-Mil <114938475+Kate-Mil@users.noreply.github.com>
2023-08-25 21:08:10 +0300

    style: make styles of LoadingLimitsList

--- src/components/LoadingLimitsList/LoadingLimitsList.js
+++ src/components/LoadingLimitsList/LoadingLimitsList.js
@@ -1,19 +1,47 @@
+import { useState } from 'react';
+import sprite from '../../pictures/sprite.svg';
+import {
+  Title,
+  List,
+  Item,
+  Wrapper,
+  Container,
+  CloseBtn,
+  Img,
+} from './LoadingLimitsList.styled';
+
 export const LoadingLimitsList = ({ onClick }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   const handleButtonClicked = limit => {
     onClick(limit);
   };
 
   const limits = [5, 10, 15, 20];
 
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <ul>
-      {limits.map(limit => (
-        <li key={limit}>
-          <button onClick={() => handleButtonClicked(limit)}>
-            Limit: {limit}
-          </button>
-        </li>
-      ))}
-    </ul>
+    <Container>
+      <Wrapper onClick={toggleDropdown}>
+        <Title>Limit</Title>
+        <CloseBtn type="button" isOpen={isOpen} onClick={toggleDropdown}>
+          <Img width="12" height="12">
+            <use href={`${sprite}#dropdown`}></use>
+          </Img>
+        </CloseBtn>
+      </Wrapper>
+      <List isOpen={isOpen}>
+        {limits.map(limit => (
+          <li key={limit}>
+            <Item onClick={() => handleButtonClicked(limit)}>
+              Limit: {limit}
+            </Item>
+          </li>
+        ))}
+      </List>
+    </Container>
   );
 };
---
+++ src/components/LoadingLimitsList/LoadingLimitsList.styled.jsx
@@ -0,0 +1,60 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  width: 101px;
+  height: 40px;
+  border-radius: 10px;
+  background: #f8f8f7;
+  position: relative;
+  cursor: pointer;
+`;
+
+export const Title = styled.h1`
+  color: #8c8c8c;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 24px;
+  padding-left: 10px;
+  padding-top: 8px;
+  padding-bottom: 8px;
+  margin: 0;
+`;
+
+export const CloseBtn = styled.button`
+  position: absolute;
+  top: 14px;
+  right: 14px;
+  color: #8c8c8c;
+  ${({ isOpen }) => isOpen && `transform: rotate(-180deg);`}
+`;
+
+export const Img = styled.svg`
+  fill: currentColor;
+`;
+
+export const List = styled.ul`
+  position: absolute;
+  top: 27%;
+  left: auto;
+  padding-left: 10px;
+  background: #f8f8f7;
+  width: 101px;
+  border-radius: 10px;
+
+  display: ${({ isOpen }) => (isOpen ? 'block' : 'none')};
+`;
+
+export const Item = styled.button`
+  color: #8c8c8c;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 24px;
+`;
