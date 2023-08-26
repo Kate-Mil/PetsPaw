3d089fc93ea5d5e33394e3abc47ddf222b6f846e
Kate-Mil <114938475+Kate-Mil@users.noreply.github.com>
2023-08-26 16:16:19 +0300

    chore: correct pushing

--- src/pages/Gallery/Gallery.js
+++ src/pages/Gallery/Gallery.js
@@ -8,27 +8,25 @@ const Gallery = () => {
   const [breeds, setBreeds] = useState([]);
   const [isloading, setIsloading] = useState(false);
   const [error, setError] = useState(null);
-  const [limit, setLimit] = useState('10');
-  const [breedId, setBreedId] = useState('abys'); //??? не срабатывает запрос
-  const [order, setOrder] = useState('RAND');
-  const [type, setType] = useState('Animated'); //????
-  const [page, setPage] = useState(0);
-  const [has_breeds, setHas_breeds] = useState(1);
+  // const [limit, setLimit] = useState('10');
+  // const [breedId, setBreedId] = useState('abys'); //??? не срабатывает запрос
+  // const [order, setOrder] = useState('RAND');
+  // const [type, setType] = useState('Animated'); //????
+  // const [page, setPage] = useState(0);
+  // const [has_breeds, setHas_breeds] = useState(1);
 
   useEffect(() => {
     async function fetchData() {
       setIsloading(true);
-      console.log(breedId);
       try {
-        const data = await getAllCats(
-          limit,
-          order,
-          has_breeds,
-          type,
-          page,
-          breedId
-        ); // Изменили на getAllCats
-        console.log('Fetch AllCats', data); // Изменили на Fetch AllCats
+        const data = await getAllCats();
+        // limit,
+        // order,
+        // has_breeds,
+        // type,
+        // page,
+        // breedId
+        console.log('Fetch AllCats', data);
         setBreeds(data);
       } catch (error) {
         setError(error.message);
@@ -38,7 +36,7 @@ const Gallery = () => {
     }
 
     fetchData();
-  }, [order, type, breedId, limit, page, has_breeds]);
+  }, []);
 
   return (
     <div>
