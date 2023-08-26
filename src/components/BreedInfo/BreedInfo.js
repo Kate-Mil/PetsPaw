a3e5e21c7ad218a493e9012cc27ea2a7e8a23acd
Kate-Mil <114938475+Kate-Mil@users.noreply.github.com>
2023-08-26 12:19:28 +0300

    make scroll for image gallery in BreedInfo

--- src/components/BreedInfo/BreedInfo.js
+++ src/components/BreedInfo/BreedInfo.js
@@ -1,17 +1,43 @@
+import { useState } from 'react';
+
 export const BreedInfo = ({ breedInfo }) => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const handleNextImage = () => {
+    if (currentImageIndex < breedInfo.length - 1) {
+      setCurrentImageIndex(currentImageIndex + 1);
+    }
+  };
+
+  const handlePreviousImage = () => {
+    if (currentImageIndex > 0) {
+      setCurrentImageIndex(currentImageIndex - 1);
+    }
+  };
+
   const [data] = breedInfo.map(el => el.breeds);
   const { name, description, temperament, origin, weight, life_span } = data[0];
 
   return (
     <>
-      <div>
-        <ul>
-          {breedInfo.map(({ id, url }) => (
-            <li key={id}>
-              <img src={url} alt="" />
-            </li>
-          ))}
-        </ul>
+      <div style={{ width: '640px', padding: '20px' }}>
+        <button
+          onClick={handlePreviousImage}
+          disabled={currentImageIndex === 0}
+        >
+          Previous
+        </button>
+        <img
+          style={{ width: '100%', height: 'avto', objectFit: 'cover' }}
+          src={breedInfo[currentImageIndex].url}
+          alt=""
+        />
+        <button
+          onClick={handleNextImage}
+          disabled={currentImageIndex === breedInfo.length - 1}
+        >
+          Next
+        </button>
       </div>
 
       <h1>{name}</h1>
