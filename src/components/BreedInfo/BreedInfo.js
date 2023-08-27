fea814931a503794250469d539d2aabb8861890b
Kate-Mil <114938475+Kate-Mil@users.noreply.github.com>
2023-08-27 12:20:07 +0300

    : style: add some settings for slider

--- src/components/BreedInfo/BreedInfo.js
+++ src/components/BreedInfo/BreedInfo.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
+// import SimpleSlider from 'components/Slider/Slider';
 
 export const BreedInfo = ({ breedInfo }) => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
