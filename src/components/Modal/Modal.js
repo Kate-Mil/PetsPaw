fdefb1fb824491c7b894b3616e5c673a73fb2380
Kate-Mil <114938475+Kate-Mil@users.noreply.github.com>
2023-08-28 08:44:07 +0300

    feat: make  drope Zone of the modal, add styled

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,22 +1,58 @@
-import { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDropzone } from 'react-dropzone';
 import { createPortal } from 'react-dom';
-import { Overlay, ModalContent } from './Modal.styled';
+import {
+  Overlay,
+  ModalContent,
+  Thumb,
+  ThumbInner,
+  ThumbImage,
+  ThumbsContainer,
+  ModalContentBnt,
+  ModalContentText,
+  ModalContentTextSpan,
+  Span,
+  ModalContentTitle,
+  ImageOverlay,
+  Img,
+  Wrapper,
+  BntContaiter,
+} from './Modal.styled';
+import sprite from '../../pictures/sprite.svg';
 
-const modalRoot = document.querySelector('#root');
+const Modal = ({ onClick }) => {
+  const [files, setFiles] = useState([]);
+  const { getRootProps, getInputProps, open } = useDropzone({
+    accept: 'image/*',
+    onDrop: acceptedFiles => {
+      setFiles(
+        acceptedFiles.map(file =>
+          Object.assign(file, {
+            preview: URL.createObjectURL(file),
+          })
+        )
+      );
+    },
+  });
 
-const Modal = ({ modalData, onClick }) => {
+  const removeFile = file => () => {
+    const newFiles = [...files];
+    newFiles.splice(newFiles.indexOf(file), 1);
+    setFiles(newFiles);
+  };
+
+  const thumbs = files.map(file => (
+    <Thumb key={file.name}>
+      <ThumbInner>
+        <ThumbImage src={file.preview} alt={file.name} />
+      </ThumbInner>
+      <button onClick={removeFile(file)}>Remove File</button>
+    </Thumb>
+  ));
+
   useEffect(() => {
-    const hendleKeyDown = e => {
-      if (e.code === 'Escape') {
-        onClick();
-      }
-    };
-    window.addEventListener('keydown', hendleKeyDown);
-
-    return () => {
-      window.removeEventListener('keydown', hendleKeyDown);
-    };
-  }, [onClick]);
+    files.forEach(file => URL.revokeObjectURL(file.preview));
+  }, [files]);
 
   const hendleOverlayClick = e => {
     if (e.currentTarget === e.target) {
@@ -27,10 +63,42 @@ const Modal = ({ modalData, onClick }) => {
   return createPortal(
     <Overlay onClick={hendleOverlayClick}>
       <ModalContent>
-        <>RERECB</>
+        <div {...getRootProps({ className: 'dropzone' })}>
+          <input {...getInputProps()} />
+          <Wrapper>
+            <ModalContentTitle>
+              Upload a .jpg or .png Cat Image
+            </ModalContentTitle>
+
+            <ModalContentText>
+              Any uploads must comply with the <Span>upload guidelines</Span> or
+              face deletion.
+            </ModalContentText>
+
+            <ImageOverlay>
+              <Img width="20" height="20">
+                <use href={`${sprite}#pload-2`}></use>
+              </Img>
+            </ImageOverlay>
+            <BntContaiter>
+              <ModalContentBnt type="button" onClick={open}>
+                <ModalContentTextSpan>Drag here </ModalContentTextSpan>your file
+                or
+              </ModalContentBnt>
+              <ModalContentBnt
+                type="button"
+                onClick={() => console.log('HIT!')}
+              >
+                <ModalContentTextSpan>Click here</ModalContentTextSpan> to
+                upload{' '}
+              </ModalContentBnt>
+            </BntContaiter>
+          </Wrapper>
+        </div>
+        <ThumbsContainer>{thumbs}</ThumbsContainer>{' '}
       </ModalContent>
     </Overlay>,
-    modalRoot
+    document.querySelector('#root')
   );
 };
 
--- src/components/Modal/Modal.styled.jsx
+++ src/components/Modal/Modal.styled.jsx
@@ -3,16 +3,116 @@ import styled from 'styled-components';
 export const Overlay = styled.div`
   position: fixed;
   top: 0;
-  left: 0;
+  right: 0;
   width: 100vw;
   height: 100vh;
   display: flex;
-  justify-content: center;
-  align-items: center;
+  justify-content: flex-end;
+  align-items: flex-end;
   background-color: #1d1d1d99;
 `;
 
 export const ModalContent = styled.div`
-  max-width: calc(100vw - 48px);
   max-height: calc(100vh - 24px);
+  border-radius: 20px;
+  background: #f8f8f7;
+  width: 680px;
+  height: 840px;
+  margin-right: 30px;
+  overflow: hidden;
+  text-align: center;
+`;
+
+export const ThumbsContainer = styled.div`
+    display: flex,
+    flex-direction: row,
+    flex-wrap: wrap,
+    margin-top: 16
+  }`;
+
+export const Thumb = styled.div`
+  display: inline-flex;
+  border-radius: 2px;
+  border: 1px solid #eaeaea;
+  margin-bottom: 8px;
+  margin-right: 8px;
+  width: 100px;
+  height: 100px;
+  padding: 4px;
+  box-sizing: border-box;
+`;
+
+export const ThumbInner = styled.div`
+  display: flex;
+  min-width: 0;
+  overflow: hidden;
+`;
+
+export const ThumbImage = styled.img`
+  display: block;
+  width: auto;
+  height: 100%;
+`;
+
+export const ModalContentBnt = styled.button`
+  color: #8c8c8c;
+  font-size: 20px;
+  font-weight: 400;
+  line-height: 30px;
+  white-space: nowrap;
+`;
+export const BntContaiter = styled.div`
+  display: flex;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+export const ModalContentTextSpan = styled.span`
+  color: #1d1d1d;
+  font-size: 20px;
+  font-weight: 500;
+  line-height: 30px;
+`;
+
+export const ModalContentTitle = styled.h1`
+  ont-size: 36px;
+  font-weight: 500;
+  display: block;
+  text-align: center;
+  margin-top: 100px;
+  margin-bottom: 10px;
+`;
+
+export const ModalContentText = styled.p`
+  margin-bottom: 40px;
+  color: #8c8c8c;
+  font-size: 20px;
+  font-weight: 400;
+  line-height: 30px;
+`;
+
+export const Span = styled(ModalContentBnt)`
+  color: #ff868e;
+`;
+
+export const ImageOverlay = styled.div`
+  border-radius: 20px;
+  border: 2px dashed #fbe0dc;
+  background: #fff;
+  width: 640px;
+  height: 320px;
+  margin: 0 auto;
+  position: relative;
+`;
+
+export const Img = styled.svg`
+  width: 100%;
+  heigt: auto;
+  object-fit: cover;
+`;
+
+export const Wrapper = styled.div`
+  position: relative;
 `;
